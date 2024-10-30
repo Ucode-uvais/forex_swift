25510069433713e413bce34f9465a37254051a59
SEYYED UVAIS <156394395+Ucode-uvais@users.noreply.github.com>
2024-10-30 08:21:56 +0530

    Update SelectCurrency.jsx

--- src/components/SelectCurrency.jsx
+++ src/components/SelectCurrency.jsx
@@ -171,7 +171,7 @@ const SelectCurrency = ({ selectedCurrency, handleCurrency }) => {
       <img
         src={`https://flagcdn.com/h40/${countryCode}.png`}
         alt="Flag"
-        className="w-8"
+        className="w-8 h-8"
       />
       <select
         className="outline-none bg-transparent text-white text-base md:text-lg font-medium w-full pr-8 cursor-pointer"
